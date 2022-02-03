65ff454a4caaef615646e3380c2e6130a0327505
Paweł Kotiuk <kotiuk@zohomail.eu>
2022-02-03 20:37:07 +0100

    base for theme change

--- src/main.cpp
+++ src/main.cpp
@@ -384,6 +384,16 @@ int main(int argc, char *argv[])
 
 #if defined(Q_OS_WIN)
     qApp->setStyle("fusion");
+    QPalette p;
+    p = qApp->palette();
+    p.setColor(QPalette::Window, QColor(53, 53, 53));
+    p.setColor(QPalette::Button, QColor(53, 53, 53));
+    p.setColor(QPalette::Base, QColor(170, 170, 170));
+    p.setColor(QPalette::Highlight, QColor(142, 45, 197));
+    p.setColor(QPalette::ButtonText, QColor(255, 255, 255));
+    p.setColor(QPalette::WindowText, QColor(255, 255, 255));
+    qApp->setPalette(p);
+    // TODO fix manually used colors in codebase and change controller image
 #endif
 
     antimicrox.setQuitOnLastWindowClosed(false);
